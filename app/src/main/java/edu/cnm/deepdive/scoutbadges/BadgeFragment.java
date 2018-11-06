d29778383c4fbe48efa74594e95c0753bb200787
Dina <dmrabanal@gmail.com>
2018-11-06 11:55:31 -0700

    adding basics for databases.

--- app/src/main/java/edu/cnm/deepdive/scoutbadges/BadgeFragment.java
+++ app/src/main/java/edu/cnm/deepdive/scoutbadges/BadgeFragment.java
@@ -1,33 +1,38 @@
 package edu.cnm.deepdive.scoutbadges;
 
-import android.content.Context;
-import android.net.Uri;
+
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.Button;
 
 
-public class BadgeFragment extends android.support.v4.app.Fragment {
+
+public class BadgeFragment extends Fragment {
 
 
-  public BadgeFragment() {
+
+    private static final String TAG = "BadgeFragment";
+
+    private Button btnNavBadge;
+
+
+    private String Badge;
+
+
+    public BadgeFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+        Bundle savedInstanceState) {
+      // Inflate the layout for this fragment
+      View view = inflater.inflate(R.layout.fragment_badges, container, false);
+
+      return view;
+    }
+
 
   }
-
-
-  @Override
-
-  public View onCreateView(LayoutInflater inflater, ViewGroup container,
-      Bundle savedInstanceState) {
-
-    View view = inflater.inflate(R.layout.fragment_badges, container, false);
-
-    return view;
-
-  }
-
-  }
-
